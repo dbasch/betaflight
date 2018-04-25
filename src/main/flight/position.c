fa0f323c5ca58c75016bcbc2a475f981fe157f65
Diego Basch <dbasch@gmail.com>
2018-04-24 21:00:29 -0700

    removed unused includes

--- src/main/flight/position.c
+++ src/main/flight/position.c
@@ -27,16 +27,9 @@
 
 #include "build/debug.h"
 
-#include "common/axis.h"
 #include "common/maths.h"
 #include "common/utils.h"
 
-#include "pg/pg.h"
-#include "pg/pg_ids.h"
-
-#include "fc/config.h"
-#include "fc/rc_controls.h"
-#include "fc/rc_modes.h"
 #include "fc/runtime_config.h"
 
 #include "flight/position.h"
@@ -44,8 +37,6 @@
 #include "flight/pid.h"
 
 #include "io/gps.h"
-
-#include "rx/rx.h"
 
 #include "sensors/sensors.h"
 #include "sensors/barometer.h"
